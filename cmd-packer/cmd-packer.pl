cef2ac6c6349e20e107cd8eda73e2f1cbba730e6
Christian Herold <harry@harry-K7VT6.(none)>
2012-12-09 16:05:07 +0100

    Implemented start routine and tar.gz, tar.bz2

--- cmd-packer/cmd-packer.pl
+++ cmd-packer/cmd-packer.pl
@@ -1,6 +1,14 @@
 #!/usr/bin/perl
-#support:
-# tar, tar.gz, tar.bz2, gz, bz2 ,zip, rar,
+# cmd-packer [-c ]/[-x] src dest
+#supports:
+# tar 
+# tar.gz
+# tar.bz2
+#next support:
+# gz 
+# bz2
+# zip
+# rar
 use strict;
 use warnings;
 use Data::Dumper;
@@ -9,7 +17,12 @@ sub xtar($$)
 {
   my $src = shift;
   my $dest = shift;
-  my @cmd =("tar","xvf",);
+  my @cmd =("tar",);
+  if( defined $_[2] ) {
+	push(@cmd,"xfv".$_[2]);
+  } else {
+	push(@cmd,"xfv");
+  }
   unless(-e $src) {
     print "No file found\n";
     exit 0;
@@ -29,26 +42,68 @@ sub xtar($$)
 }
 sub ctar ($$)
 {
-  my @cmd = ("tar","cvf",$_[1],$_[0]);
+  my @cmd = ("tar",);
+  my $src = shift;
+  my $dest = shift;
+  unless(-e $src || -d $src) {
+    print "No file/directory found\n";
+    exit 0;
+  }
+  if( defined $_[2] ) {
+    push(@cmd,"cfv".$_[2]);
+  } else {
+    push(@cmd,"cfv");
+  }
+  push(@cmd, $dest);
+  push(@cmd, $src);
   system(@cmd) == 0
     or die "cmd  @cmd failed: $?";
 }
-sub xtargz($$)
-{
-  print "extract tar.gz\n";
+
+if( @ARGV != 3 ){
+  print "Wrong argument count\n";
+  exit 0;
 }
-sub ctargz($$)
-{
-  print "compress tar.gz\n";
+my $func = $ARGV[0];
+
+if( length($func) != 2 || ( $func ne "-c" && $func ne "-x" ) ) {
+  print "Wrong Option choose between -c and -x\n";
+  exit 0;
 }
 my %archives = (
   "tar" => {
-    "extract" => \&xtar,
-    "compress" => \&ctar,
-  },
-  "tar.gz" => {
-    "extract" => \&xtargz,
-    "compress" => \&ctargz,
+    "-x" => \&xtar,
+    "-c" => \&ctar,
   },
 );
-$archives{"tar"}{"extract"}("hallo.tar","bier");
+my $src = $ARGV[1];
+my $dest = $ARGV[2];
+my @ext;
+if( $func eq "-c" ) {
+  if ($dest =~ m/^.*\..*/) {
+    @ext = split(/\./,$dest,3);
+  } else {
+    print "No extention found\n";
+    exit 0;
+  }
+}
+elsif( $func eq "-x" ) {
+  if ($src =~ m/^.*\..*/) {
+    @ext = split(/\./,$src,3);
+  } else {
+    print "No extention found\n";
+    exit 0;
+  }
+} else {
+  print "Wrong file type\n";
+  exit 0;
+}
+if( exists $archives{$ext[1]} ) {
+  if( scalar(@ext) == 3 && $ext[1] eq "tar" ) {
+    $_ = ($ext[2] eq "gz" || $ext[2] eq "bz2")?
+    ($ext[2] eq "gz") ? "z" : "j" :undef;
+    $archives{$ext[1]}{$func}($src,$dest,$_);
+  } else {
+    $archives{$ext[1]}{$func}($src,$dest);
+  }
+}
